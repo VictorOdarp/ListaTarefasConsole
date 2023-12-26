9ace821a5c5d4b82dc1698afa42185a7b107a1b4
Victor Eduardo <VictorOdarp@gmail.com>
2023-12-26 20:53:38 -0300

    Implementado o básico de login (falta validações), listagem de usuários, Cadastrado de usuários

---
+++ ListaDeTarefas/ListaDeTarefas/Entities/User.cs
@@ -0,0 +1,97 @@
+﻿
+
+using System.Reflection.Metadata;
+
+namespace ListaDeTarefas.Entities
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<User> Users { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public void CadastrarUsuario(List<User> users)
+        {
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            while(!email.Contains("@"))
+            {
+                Console.WriteLine("Email is not valid!");
+                email = Console.ReadLine();
+            }
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            Console.Write("Confirm your password: ");
+            string confirmPassoword = Console.ReadLine();
+            while (confirmPassoword != password)
+            {
+                Console.WriteLine("Passwords don't match!");
+                Console.Write("Confirm your password: ");
+                confirmPassoword = Console.ReadLine();
+            }
+            User user = new User(name, email, password);
+            users.Add(user);
+            Console.WriteLine();
+            
+        }
+        public void FazerLogin(List<User> users)
+        {
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+ 
+            while (!users.Any(x => x.Email == email))
+            {
+                Console.WriteLine("Email não cadastrado");
+                Console.WriteLine();
+                
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+            
+            }
+
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+
+            while (!users.Any(x => x.Password == password))
+            {
+                Console.WriteLine("Senha incorreta");
+                Console.WriteLine();
+
+                Console.Write("Password: ");
+                password = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bem vindo a sua lista de tarefas!");
+            Console.WriteLine();
+       
+       
+        }
+
+        public void ListarUsuários(List<User> users) 
+        {
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(users.Count() + " - " + user.Name + " - " + user.Email);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ ListaDeTarefas/ListaDeTarefas/Program.cs
@@ -0,0 +1,48 @@
+﻿using ListaDeTarefas.Entities;
+using System;
+
+namespace ListaDeTarefas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            User user = new User();
+
+            Console.WriteLine("Bem vindo a sua Lista de Tarefas!");
+            Console.WriteLine();
+            Inicio(users, user);
+
+            static void Inicio(List<User> users, User user)
+            {
+
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Cadastrar usuário");
+                Console.WriteLine("2 - Fazer login");
+                Console.WriteLine("3 - Listar usuários");
+                Console.WriteLine();
+
+                Console.Write("Digite a sua opção: ");
+                int validação = int.Parse(Console.ReadLine());
+
+                if (validação == 1)
+                {
+                    user.CadastrarUsuario(users);
+                    Inicio(users, user);
+                }
+                if (validação == 2)
+                {
+                    user.FazerLogin(users);
+                }
+                if (validação == 3)
+                {
+                    user.ListarUsuários(users);
+                    Inicio(users, user);
+                }
+            }
+
+
+        }
+    }
+}
